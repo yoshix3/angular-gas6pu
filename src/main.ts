c8b05fe899e6e004c7ea452c6a7230160e9e8d30
yoshix3 <37896333+yoshix3@users.noreply.github.com>
2023-03-28 22:26:45 +0900

    migration

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
 
-import { ChartsModule } from 'ng2-charts';
-import { ChartDataSets, ChartOptions, ChartType } from 'chart.js'; // データ型をimport
-import { Color, Label } from 'ng2-charts'; // ng2-chartsのプロパティのデータ型をimport
+import { NgChartsModule } from 'ng2-charts';
+import { ChartDataset, ChartOptions, ChartType } from 'chart.js'; // データ型をimport
+// import { Color, Label } from 'ng2-charts'; // ng2-chartsのプロパティのデータ型をimport
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule, ChartsModule],
+  imports: [CommonModule, NgChartsModule],
   template: `
     <h1>Hello from {{name}}!</h1>
     <a target="_blank" href="https://angular.io/start">
@@ -22,9 +22,8 @@ import { Color, Label } from 'ng2-charts'; // ng2-chartsのプロパティのデ
         [datasets]="lineChartData"
         [labels]="lineChartLabels"
         [options]="lineChartOptions"
-        [colors]="lineChartColors"
         [legend]="lineChartLegend"
-        [chartType]="lineChartType"
+        [type]="lineChartType"
         [plugins]="lineChartPlugins">
       </canvas>
     </div>
@@ -34,10 +33,9 @@ import { Color, Label } from 'ng2-charts'; // ng2-chartsのプロパティのデ
         [datasets]="barChartData"
         [labels]="barChartLabels"
         [options]="barChartOptions"
-        [colors]="barChartColors"
         [plugins]="barChartPlugins"
         [legend]="barChartLegend"
-        [chartType]="barChartType">
+        [type]="barChartType">
       </canvas>
     </div>
 
@@ -47,90 +45,117 @@ export class App {
   name = 'Angular';
 
   // data
-  lineChartData: ChartDataSets[] = [
+  lineChartData: ChartDataset[] = [
     {
       data: [100, 60, 90, 20, 80, 50],
       label: '精度',
-      lineTension: 0,
+      // lineTension: 0,
+      tension: 0,
     },
     {
       data: [50, 50, 50, 50, 50, 50],
       label: '閾値',
-      lineTension: 0,
+      // lineTension: 0,
+      tension: 0,
     },
   ];
 
   // ラベル
-  lineChartLabels: Label[] = ['1', '2', '3', '4', '5', '6'];
+  // lineChartLabels: Label[] = ['1', '2', '3', '4', '5', '6'];
+  lineChartLabels: string[] = ['1', '2', '3', '4', '5', '6'];
 
   lineChartOptions: ChartOptions = {
     responsive: true,
     maintainAspectRatio: false,
     scales: {
-      xAxes: [
-        {
-          ticks: {
-            min: 1,
-            max: 100,
-            precision: 0,
+      x: {
+        min: 1,
+        max: 100,
+        ticks: {
+          precision: 0,
+        },
+      },
+      y: {
+        min: 0,
+        ticks: {
+          precision: 0,
+          callback: function (value) {
+            return value + '%';
           },
         },
-      ],
-      yAxes: [
-        {
-          ticks: {
-            min: 0,
-            precision: 0,
-            callback: function (value) {
-              return value + '%';
-            },
-          },
-        },
-      ],
+      },
+
+      // xAxes: [
+      //   {
+      //     ticks: {
+      //       min: 1,
+      //       max: 100,
+      //       precision: 0,
+      //     },
+      //   },
+      // ],
+      // yAxes: [
+      //   {
+      //     ticks: {
+      //       min: 0,
+      //       precision: 0,
+      //       callback: function (value) {
+      //         return value + '%';
+      //       },
+      //     },
+      //   },
+      // ],
     },
     plugins: {
-      datalabels: {
-        display: false,
-      },
+      // datalabels: {
+      //   display: false,
+      // },
     },
   };
 
   // 色
-  lineChartColors: Color[] = [
-    {
-      borderColor: 'blue',
-      backgroundColor: 'rgba(0,0,255,0.28)',
-    },
-    {
-      borderColor: 'green',
-      backgroundColor: 'rgba(0,0,255,0)',
-    },
-  ];
+  // lineChartColors: Color[] = [
+  //   {
+  //     borderColor: 'blue',
+  //     backgroundColor: 'rgba(0,0,255,0.28)',
+  //   },
+  //   {
+  //     borderColor: 'green',
+  //     backgroundColor: 'rgba(0,0,255,0)',
+  //   },
+  // ];
 
   lineChartLegend = true; // グラフの属性ラベル
   lineChartPlugins = [];
-  lineChartType = 'line'; // グラフの種類
+  lineChartType: ChartType = 'line'; // グラフの種類
 
   barChartOptions: ChartOptions = {
     responsive: true,
     scales: {
-      xAxes: [
-        {
-          ticks: {
-            min: 0,
-            precision: 0,
-          },
+      x: {
+        min: 0,
+        ticks: {
+          precision: 0,
         },
-      ],
+      },
+      // xAxes: [
+      //   {
+      //     ticks: {
+      //       min: 0,
+      //       precision: 0,
+      //     },
+      //   },
+      // ],
     },
     plugins: {
-      datalabels: {
-        anchor: 'end',
-        align: 'end',
-      },
+      // datalabels: {
+      //   anchor: 'end',
+      //   align: 'end',
+      // },
     },
   };
-  barChartLabels: Label[] = [
+  // barChartLabels: Label[] = [
+  barChartLabels: string[] = [
     'January',
     'February',
     'March',
@@ -138,19 +163,20 @@ export class App {
     'May',
     'June',
   ];
-  barChartType: ChartType = 'horizontalBar';
+  // barChartType: ChartType = 'horizontalBar';
+  barChartType: ChartType = 'bar';
   barChartLegend = true;
   barChartPlugins = [];
 
-  barChartData: ChartDataSets[] = [
+  barChartData: ChartDataset[] = [
     { data: [100, 200, 70, 600, 450, 300], label: '月別降水量' },
   ];
   // 色
-  barChartColors: Color[] = [
-    {
-      backgroundColor: 'rgba(0,0,255,0.28)',
-    },
-  ];
+  // barChartColors: Color[] = [
+  //   {
+  //     backgroundColor: 'rgba(0,0,255,0.28)',
+  //   },
+  // ];
 }
 
 bootstrapApplication(App);
